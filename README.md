64ec0b734b4916b9b94f2428c8ed6c5ec2f609ce
mgl <1375022007@qq.com>
2022-05-29 21:41:01 +0800

    init repository add some file

---
+++ README.md
@@ -0,0 +1,6 @@
+## learn jest  
+
+记录jest学习过程中的一些demo
+
+### 参考资料
+- https://github.yanhaixiang.com/jest-tutorial/
---
+++ tests/utils/sum.test.js
@@ -0,0 +1,5 @@
+const sum=require('../../src/utils/sum');
+
+it('test sum function',()=>{
+  expect(sum(1,2)).toBe(3)
+})
