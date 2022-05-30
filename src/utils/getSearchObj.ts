ebfbdd6391b6d99daa1330305c5ae3ca80286f7f
mgl <1375022007@qq.com>
2022-05-30 09:41:36 +0800

    add mock 网页地址

---
+++ src/utils/getSearchObj.ts
@@ -0,0 +1,18 @@
+const getSearchObj=()=>{
+  const {search}=window.location;
+  const searchStr=search.slice(1);
+  const pairs = searchStr.split("&");
+
+  // { 'a': '1' }
+  const searchObj: Record<string, string> = {};
+
+  pairs.forEach((pair) => {
+    // [a, 1]
+    const [key, value] = pair.split("=");
+    searchObj[key] = value;
+  });
+
+  return searchObj;
+}
+
+export default getSearchObj;
